d1ddf00dd446f1fc6de848bb68c260ce137bf066
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-02-08 15:13:42 +0900

    Add chat2_exercise.java

--- chat2_exercise/exercise/src/Exercise2.java
+++ chat2_exercise/exercise/src/Exercise2.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Exercise2 {
-    
+    public Exercise2() {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        System.out.print("2자리 정수 입력(10~99): ");
+        a = sc.nextInt();
+
+        b = a%10;
+        if((a/10 != b)){
+            System.out.println("N0! 10의 자리와 1의 자리가 같지 않습니다");
+        }
+        else
+            System.out.println("YES! 10의 자리와 1의 자리가 같습니다");
+
+    }
 }
---
+++ chat2_exercise/exercise/src/Exercise3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+    public Exercise3() {
+        Scanner sc = new Scanner(System.in);
+        int a;
+
+        System.out.print("금액을 입력하세요>> ");
+        a = sc.nextInt();
+
+        System.out.print("오만원권 " +a/50000+ '매');
+//        System.out.print()
+//        System.out.print()
+//        System.out.print()
+//        System.out.print()
+//        System.out.print()
+
+    }
+}
