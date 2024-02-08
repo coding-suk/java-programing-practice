c45b0203d9c5c57fc809de38631cc84eac330dc7
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-02-08 15:44:29 +0900

    Modify chat2_exercise.java

--- chat2_exercise/exercise/src/Exercise3.java
+++ chat2_exercise/exercise/src/Exercise3.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Exercise3 {
     public Exercise3() {
         Scanner sc = new Scanner(System.in);
-        int a;
+        int a,b;
 
         System.out.print("금액을 입력하세요>> ");
         a = sc.nextInt();
 
-        System.out.print("오만원권 " +a/50000+ '매');
-//        System.out.print()
-//        System.out.print()
-//        System.out.print()
-//        System.out.print()
-//        System.out.print()
-
+        System.out.println("오만원권 " +a/50000+ '매');
+        a %= 50000;
+        System.out.println("만원권 " + a/10000+ '매');
+        a %= 10000;
+        System.out.println("천원권 " +a/1000+ '매');
+        a %= 1000;
+        System.out.println("백원 " +a/100+ '매');
+        a %= 100;
+        System.out.println("오십원 " +a/50+ '매');
+        a %= 50;
+        System.out.println("십원 " +a/10+ '매');
+        a %= 10;
+        System.out.println("일원 " +a+ '매');
     }
 }
