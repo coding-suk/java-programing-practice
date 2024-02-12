575f3ed73f7ba8c043c9f0d99a04187133db3224
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-02-12 17:46:02 +0900

    Add chat3_exercise.java

--- chat3_exercise/chat3_exercise/src/Main.java
+++ chat3_exercise/chat3_exercise/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //new open_chellange();
-        new modify_chellange();
+        //new modify_chellange();
+        //new exercise1();
+        new exercise2();
     }
 }
---
+++ chat3_exercise/chat3_exercise/src/exercise1.java
@@ -0,0 +1,12 @@
+public class exercise1 {
+    public exercise1() {
+        int sum, i;
+        sum = 0;
+        i = 0;
+        while(i < 100) {
+            sum += i;
+            i += 2;
+        }
+        System.out.println(sum);
+    }
+}
