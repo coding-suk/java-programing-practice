a1bc47c5680238aafaf33429cfc6abf8abd677ab
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-02-17 16:00:13 +0900

    modify chat3_exercise5

--- chat3_exercise/chat3_exercise/src/exercise5.java
+++ chat3_exercise/chat3_exercise/src/exercise5.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 public class exercise5 {
     public exercise5() {
         Scanner sc = new Scanner(System.in);
-        int Array[];
-        int a;
-        Array = new int[10];
+        int n[] = new int[10];
 
         System.out.print("양의 정수 10개를 입력하시오>> ");
         for (int i = 0; i < 10; i++) {
-            Array[i] = sc.nextInt();
+            n[i] = sc.nextInt();
         }
         System.out.print("3의 배수는 ");
         for (int i = 0; i < 10; i++) {
-            if (Array[i] % 3 == 0) {
-                System.out.print(Array[i]);
+            if (n[i] % 3 == 0) {
+                System.out.print(n[i]);
                 System.out.println();
             }
         }
