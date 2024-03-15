873c8bd66e0f042f58ce80a7430fe73b11851e90
developer1 <dev1@email.com>
2024-03-16 02:45:43 +0900

    Modify Circle

---
+++ chat4_example/src/Circle.java
@@ -0,0 +1,32 @@
+public class Circle {
+    int radius;
+    String name;
+    public Circle() {
+        CircleAp pizza = new CircleAp(10, "자바피자");
+
+        double area = pizza.getArea();
+        System.out.println(pizza.name + "의 면적은 " + area);
+
+        CircleAp donut = new CircleAp();
+        donut.name = "도넛 피자";
+        area = donut.getArea();
+        System.out.println(donut.name + "의 면적은 " + area);
+    } // 생성자
+
+    class CircleAp {
+        int radius = 1;
+        String name = "";
+
+        public CircleAp() {
+            radius = 1; name = "";
+        }
+        public CircleAp(int r, String n) {
+            radius = r; name = n;
+        }
+        public double getArea() {
+            return 3.14*radius*radius;
+        }
+    }
+
+
+}
