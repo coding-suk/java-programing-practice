66599656cc9732b66ca15d752da1ccea1dca263f
developer1 <dev1@email.com>
2024-03-16 18:50:11 +0900

    Add Chat4_Main.java

--- chat4_example/src/Main.java
+++ chat4_example/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //new Figure();
-        new Circle();
-
+        //new Circle();
+        //new Book();
+        new useThis();
     }
 }
